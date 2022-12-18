1437cd29a7ae38b730500d62e894f5692db53cec
Alec Pool <alec.pool@gmail.com>
2022-12-17 21:38:44 -0500

    heroku fixes

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,6 +1,6 @@
 # Stores all configuration values
 SECRET_KEY = b')\xefC\xb6\x06\x0cT\xf7\x08\xa6O\x96I\xa6D\x9d'
-MONGODB_HOST = "mongodb://localhost:27017/388JFinal2"
+#MONGODB_HOST = "mongodb://localhost:27017/388JFinal2"
 
 
 #mongodb+srv://<username>:<password>@cluster0.zsgbhxu.mongodb.net/?retryWrites=true&w=majority
